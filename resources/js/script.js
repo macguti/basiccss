70d1942217a2cad5334fad89df10c82bbb24be38
marco antonio gutierrez casanova <macgutar@gmail.com>
2019-09-25 13:00:39 -0300

    arreglando errores

--- resources/js/script.js
+++ resources/js/script.js
@@ -87,5 +87,24 @@ $(".js--wp-1").waypoint(function(direction){
      offset:"50%"
  });
 
+ /*mobile nav*/ 
+ 
+ $(".js--nav-icons").click(function(){
+   var nav= $(".js--main-nav");
+   var icon= $(".js--nav-icons i");
+   
+    nav.slideToggle(200);
+    if(icon.hasClass("ion-navicon-round")){
+        icon.addClass("ion-close-round");
+        icon.removeClass("ion-navicon-round");
+    }else{
+        icon.addClass("ion-navicon-round");
+        icon.removeClass("ion-close-round");
+    }
+ });
+ 
+
+
+
 
 });
